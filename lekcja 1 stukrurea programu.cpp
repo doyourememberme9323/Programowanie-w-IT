a5b45e344019fbca619d201eaf205e770a1c3fef
doyourememberme9323 <pmhunter402@gmail.com/doyourememberme9323/Programowanie-w-IT>
2023-10-25 11:55:12 +0200

    Komentarz

--- lekcja 1 stukrurea programu.cpp
+++ lekcja 1 stukrurea programu.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 using namespace std;
 int main(){
-cout <<"Hello world"<<endl;
-cout <<"Imie";	
+	
+short liczba = 2 ;  // zmienna liczbowa
+float liczbauzytkownika;
+cout << "Hello world" << endl;
+cout << "Imie " << liczba; // wyswietlenie zmienne 
 
+cout << endl;  // koniec lini 
+cout <<"Podaj liczne"; 
+cin >> liczbauzytkownika; // wprowadzenie danych i zapisanie do zmiennej 
+
+cout << endl;
+cout<<"podana liczba" << liczbauzytkownika;
+
+
+/*
+komentarz
+na wiele lini 
+*/
 return 0;
 }
-
