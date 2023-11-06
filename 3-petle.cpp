f2a969143000571d53a732b501e668c92777c9f9
doyourememberme9323 <pmhunter402@gmail.com/doyourememberme9323/Programowanie-w-IT>
2023-11-06 15:08:44 +0100

    Komentarz

---
+++ 3-petle.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+/*Inkrementacja
+i = 1 + 1 
+i += 1
+i++
+Dwkramentacja
+i = i -1
+i-= 1
+i--
+*/	
+// for(wartoscPoczatkowa; warunkekKonczacy; krok)
+for(int i = 0; i < 5; i++){ // i - zmienna lokalna, istnieje tylko w petli  
+	cout << "literacja nr: " << i <<endl;
+}
+	
+int wysokosc, szerokosc;
+	cout << "Podaj szerokosc: ";
+	cin >> szerokosc;
+	cout << "Podaj wysokosc: ";
+	cin >> wysokosc;
+	for(int i = 0; i < 5; i++){
+		for(int j = 0; j << 5;j++){
+			cout << " " << i << j;
+		}
+	cout << endl;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+cout << endl;
+system ("pause");		
+}
