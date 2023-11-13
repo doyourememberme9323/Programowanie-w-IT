43f3ed4f1b7a7b75aea1f93e9c29e8c681178770
doyourememberme9323 <pmhunter402@gmail.com/doyourememberme9323/Programowanie-w-IT>
2023-11-13 15:01:04 +0100

    Komentarz

---
+++ 122 petlla.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "podaj i: ";
+	cin >> a;
+	cout << "podaj j: ";
+	cin >> ; j
+
+	for(int i=2 ; i < 5; i++ ){
+ 		for (int j=2; j < 5; j++){
+           cout << "*";
+		}  
+	cout << endl;
+    }
+}
