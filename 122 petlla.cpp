f0238763715d1667620221786bfba2f52150755a
doyourememberme9323 <pmhunter402@gmail.com/doyourememberme9323/Programowanie-w-IT>
2023-11-15 12:06:01 +0100

    Komentarz

--- 122 petlla.cpp
+++ 122 petlla.cpp
@@ -1,16 +1,35 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-	cout << "podaj i: ";
-	cin >> a;
-	cout << "podaj j: ";
-	cin >> ; j
 
-	for(int i=2 ; i < 5; i++ ){
- 		for (int j=2; j < 5; j++){
+int main(){
+
+     int szerokosc, wysokosc;
+	cout <<"Podaj wysokosc: " << endl;
+	cin >> wysokosc ;
+	cout << "podaj szerokosc: " << endl;
+	cin >> szerokosc;
+
+	for(int i=0 ; i < wysokosc; i++ ){
+ 		for (int j=0; j < szerokosc; j++){
+           if (i == 0 || i == wysokosc-1)
            cout << "*";
+           else if (j == 0 || j == szerokosc-1)
+		cout << "*";
+           else
+		       cout << " "; 
 		}  
-	cout << endl;
+	   cout << endl;
+	
+	
+	
+	 
     }
+    
+    
+    
+    
+    
 }
+
+
