ddb4497fbbc597519189d16a06f2f6f990a615a5
doyourememberme9323 <pmhunter402@gmail.com/doyourememberme9323/Programowanie-w-IT>
2023-11-22 12:02:58 +0100

    Komentarz

---
+++ petal.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using  namespace std;
+
+
+int main (){
+ 	
+	int szerokosc , wysokosc;
+	cout <<"Podaj wysokosc: " << endl;
+	cin >> wysokosc ;
+	cout << "podaj szerokosc: " << endl;
+	cin >> szerokosc;
+	
+	for(int i=0 ; i < wysokosc ; i++){
+	    for (int j=0; j < szerokosc ; j++){
+		if (i == 0 ||  i == wysokosc -1)
+		cout <<  "*";
+		else if (j == 0 || j == szerokosc -1)
+	cout << "*";
+	  else
+	     cout << " ";
+     }
+    cout << endl;	
+	
+	
+	
+
+}
+	
+	
+	
+	
+	
+}
